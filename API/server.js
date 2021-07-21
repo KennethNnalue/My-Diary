3e1376edde88a7811e3e3147e39d7d50e2e41d81
Nnalue Kenneth <56474257+KennethNnalue@users.noreply.github.com>
2021-07-21 12:41:09 +0300

    Create index route, controllers and fixing the server

--- API/server.js
+++ API/server.js
@@ -1,7 +1,10 @@
+import route from 'color-convert/route';
 import express from 'express';
+import index from './Routes';
 const server = express();
 function displayServerMessage() {
     return console.log('My Diary API server is listening on Port 8800');
 }
+server.use('/api/v1',index);
 server.listen(8800, displayServerMessage);
 export default server;
