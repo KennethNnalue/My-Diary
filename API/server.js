9548216525e68f75b48347b1ba25f9147c535790
Nnalue Kenneth <56474257+KennethNnalue@users.noreply.github.com>
2021-07-22 13:43:26 +0300

    Add Home Page API to power Home page

--- API/server.js
+++ API/server.js
@@ -1,10 +1,11 @@
-import route from 'color-convert/route';
 import express from 'express';
 import index from './Routes';
+
 const server = express();
 function displayServerMessage() {
     return console.log('My Diary API server is listening on Port 8800');
 }
-server.use('/api/v1',index);
+server.use(index);
 server.listen(8800, displayServerMessage);
+
 export default server;
