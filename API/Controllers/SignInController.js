ed1d02d05ba83e1dbf22b67597fa0db85f43be89
Nnalue Kenneth <56474257+KennethNnalue@users.noreply.github.com>
2021-07-29 14:25:09 +0300

    Add Sign In API

---
+++ API/Controllers/SignInController.js
@@ -0,0 +1,33 @@
+import bcrypt from 'bcrypt-nodejs';
+import models from '../models/index';
+import createToken from '../security/createToken';
+
+const { User } = models;
+
+class SignInController {
+  static async signIn(request, response) {
+    const { email, password } = request.body;
+
+    const userFound = await User.findOne({
+      where: {
+        email
+      }
+    });
+
+    const isUserFound = userFound && bcrypt.compareSync(password, userFound.password);
+    if (!isUserFound) {
+      return response.status(401).json({
+        message: 'Invalid Email or Password'
+      });
+    }
+
+    return response.status(200).json(
+      {
+        message: 'Your Signed In',
+        token: createToken(userFound.id, 60 * 60 * 24)
+
+      }
+    );
+  }
+}
+export default SignInController;
