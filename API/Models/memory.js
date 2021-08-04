d2cd608e6bfc3f33259e961320b3006d6ab2b040
Nnalue Kenneth <56474257+KennethNnalue@users.noreply.github.com>
2021-08-04 12:47:07 +0300

    Created the user table

---
+++ API/Models/memory.js
@@ -0,0 +1,30 @@
+
+
+const {
+  Model
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Memory extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      const { User } = Model;
+      Memory.belongsTo(User, { foreignKey: 'userId', onDelete: 'CASCADE' });
+    }
+  }
+  Memory.init({
+    title: DataTypes.STRING,
+    story: DataTypes.STRING,
+    picture: DataTypes.STRING,
+    userId: DataTypes.NUMBER
+  }, {
+    sequelize,
+    modelName: 'Memory',
+  });
+  return Memory;
+};
