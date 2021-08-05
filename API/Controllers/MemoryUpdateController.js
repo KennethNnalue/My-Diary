b26a2e3442349c4742696daab5057fd9b9ca04f7
Nnalue Kenneth <56474257+KennethNnalue@users.noreply.github.com>
2021-08-06 06:21:00 +0800

    Add update memory feature and changed the landing page diary icon

--- API/Controllers/MemoryUpdateController.js
+++ API/Controllers/MemoryUpdateController.js
@@ -1,7 +1,31 @@
+import models from '../models';
 
 class MemoryUpdateController {
   static async updateMemory(request, response) {
+    try {
+      const [[result]] = await models.sequelize.query('UPDATE "Memories" SET "title" = :title, "story" = :story, "mood" = :mood, "picture" = :picture, "userId" = :userId,  "updatedAt" = :updatedAt WHERE ("userId" = :userId) AND ("id" = :id)   RETURNING *;', {
+        replacements: {
+          userId: request.userWallet.userId,
+          id: request.params.id,
+          title: request.body.title,
+          story: request.body.story,
+          picture: request.body.picture,
+          mood: request.body.mood,
+          updatedAt: new Date(Date.now())
 
+        }
+      });
+      return response.status(200).json({
+        message: 'memory updated',
+        memory: result
+      });
+    } catch (e) {
+      return response.status(500).json({
+        message: 'Internal Sever Error',
+        error: e.toString()
+
+      });
+    }
   }
 }
 
